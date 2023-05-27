967a21d9c9235b28581b2e7271de7b925606fc5d
phshafi <shafi@programming-hero.com>
2023-05-28 02:33:09 +0600

    created fake array for mock db

---
+++ data.js
@@ -0,0 +1,14 @@
+const pets = [
+    {
+        id: 1,
+        name: "Bella",
+        type: "Dog"
+    },
+    {
+        id: 2,
+        name: "Whiskers",
+        type: "Cat"
+    }
+];
+
+module.exports = pets;
