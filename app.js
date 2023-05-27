b2195456a49f03520c138fa064f739b626229863
phshafi <shafi@programming-hero.com>
2023-05-28 02:32:43 +0600

    removed unnecessary spaces

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
 
-
 const app = express();
 app.use(express.json());
 
 app.use("/api", routes);
 
 app.listen("8080", () => {
-  console.log("Server is up and running");
+    console.log("Server is up and running");
 });
 
 module.exports = app;
